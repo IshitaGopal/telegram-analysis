094b35f4e4e46acbbe1de27a07936803adb43621
Ishita Gopal <ishitagopal@gmail.com>
2023-02-12 14:09:59 -0500

    added data utils

--- analysis/Config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    analysis_dir = "analysis"
-    proccessed_data_dir = "processed_data"
-    chat_data_subsets_subdir = "chat_data_subsets"
-    viasna_data_subdir = "viasna_data"
-    important_events_wiki_file = "important_events.csv"
-    analysis_data_file = "Aug_Mar_2023-02-04.csv"
